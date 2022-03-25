535533e2a50a2b4bff784309a24e9bfb83aba2d1
Klara Filek <98151543+kl-fil@users.noreply.github.com>
2022-03-25 14:00:37 +0100

    Add files via upload

---
+++ 16S_data/bacterial_isolates/iqtree_instructions.sh
@@ -0,0 +1,17 @@
+## Iqtree2 installation 
+## tutorial: http://www.iqtree.org/doc/Tutorial
+
+conda install -c bioconda iqtree
+
+## run iqtree2 on fasta alignment
+## use -nt AUTO for automatic determining of how many threads the program uses
+## use model finder to determine the best substitution model automatically (default)
+
+## cite: S. Kalyaanamoorthy, B.Q. Minh, T.K.F. Wong, A. von Haeseler, and L.S. Jermiin
+## (2017) ModelFinder: fast model selection for accurate phylogenetic estimates. Nat. 
+## Methods, 14:587–589. DOI: 10.1038/nmeth.4285
+
+## use ultrafast bootstrap -bb 1000
+
+iqtree -s all_seq_muscle_aligned_aliview.fas -m MFP -bb 1000 -nt AUTO
+
